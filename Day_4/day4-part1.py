827fae2199cf518b206c7dcb6b1aac83bafdb628
sdesylva <sdesylva@deloitte.co.uk>
2022-12-04 14:41:19 +0000

    Add Day 4 solutions

---
+++ Day_4/day4-part1.py
@@ -0,0 +1,13 @@
+count = 0
+with open("day4-data.txt") as list:
+    for pair in map(str.strip, list):
+        elf1,elf2 = pair.split(",",1)
+        s1,e1 = elf1.split("-",1)
+        s2,e2 = elf2.split("-",1)
+        elf1 = set(range(int(s1),int(e1)+1))
+        elf2 = set(range(int(s2),int(e2)+1))
+
+        if elf1.intersection(elf2) == elf1 or elf1.intersection(elf2) == elf2:
+            count += 1
+
+print(count)
